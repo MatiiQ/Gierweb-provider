e87d0f382507dd4d093b2a3ea99b4f916da45d59
BarHanSolo <andrzej.lippa@gmail.com>
2021-04-09 00:31:14 +0200

    cors

--- prov.js
+++ prov.js
@@ -7,27 +7,33 @@ let game_user = JSON.parse(fs.readFileSync('gameDB.json'))
 let gamesUsers = JSON.parse(fs.readFileSync('game_user-U.json'))
 
 app.get('/users', function (req, res) {
+  res.set('Access-Control-Allow-Origin', '*');
   res.send(users);
 })
 
 app.get('/users/:userId', function (req, res) {
+  res.set('Access-Control-Allow-Origin', '*');
   res.send(users.find((user) => user.id == req.params.userId));
 })
 
 
 app.get('/users/gender/:userGender', function (req, res) {
+  res.set('Access-Control-Allow-Origin', '*');
   res.send(users.filter((user) => user.gender == req.params.userGender));
 })
 
 app.get('/users/find/tidies', function (req, res) {
+  res.set('Access-Control-Allow-Origin', '*');
   res.send(users.filter((user) => user.first_name.includes('tit')));
 })
 
 app.get('/gamesUsers', function (req, res) {
+  res.set('Access-Control-Allow-Origin', '*');
   res.send(gamesUsers);
 })
 
 app.get('/gamesUsers/:userId', function (req, res) {
+  res.set('Access-Control-Allow-Origin', '*');
   res.send(users.find((gamesUsers) => gamesUsers.id_user == req.params.userId));
 })
 
