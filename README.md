258b5065942cf1351e3b16aaf37295c90c20daf7
MatiiQ <matiu.k.13@gmail.com>
2021-04-09 17:49:05 +0200

    Added new endpoints

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Gierweb-provider
 provider for gierweb app
+
+## All endpoints and examples for now:
+### ALL 
+1. Games
+  https://gier-web.herokuapp.com/games  
+
+2. Users
+  https://gier-web.herokuapp.com/users  
+
+3. Pairs of game and user IDs
+  https://gier-web.herokuapp.com/gamesUsers  
+
+### GAMES
+1. Game with specified ID
+  https://gier-web.herokuapp.com/games/1  
+
+2. Games with specified genre
+  https://gier-web.herokuapp.com/games/genres/RPG
+### USERS
+1. User with specified ID
+  https://gier-web.herokuapp.com/users/1    
+
+2. Users with specified gender
+  https://gier-web.herokuapp.com/users/gender/Polygender  
+
+### GAMES-USERS PAIRS
+1. Pairs of IDs for game and specified user (what games that user has)
+  https://gier-web.herokuapp.com/gamesUsers/user/1  
+
+2. Pairs of IDs for specified game and user (which users have that game)
+  https://gier-web.herokuapp.com/gamesUsers/game/1  
--- prov.js
+++ prov.js
@@ -4,42 +4,66 @@ const fs = require('fs')
  
 let users = JSON.parse(fs.readFileSync('User.json'))['62fbf740']
 let games = JSON.parse(fs.readFileSync('gameDB.json'))
-let gamesUsers = JSON.parse(fs.readFileSync('game_user-U.json'))
+let gamesUsers = JSON.parse(fs.readFileSync('game_user.json'))
 
+// All games
 app.get('/games', function (req, res) {
   res.set('Access-Control-Allow-Origin', '*');
   res.send(games);
 })
 
+// All users
 app.get('/users', function (req, res) {
   res.set('Access-Control-Allow-Origin', '*');
   res.send(users);
 })
 
-app.get('/users/:userId', function (req, res) {
-  res.set('Access-Control-Allow-Origin', '*');
-  res.send(users.find((user) => user.id == req.params.userId));
-})
-
-
-app.get('/users/gender/:userGender', function (req, res) {
-  res.set('Access-Control-Allow-Origin', '*');
-  res.send(users.filter((user) => user.gender == req.params.userGender));
-})
-
-app.get('/users/find/tidies', function (req, res) {
-  res.set('Access-Control-Allow-Origin', '*');
-  res.send(users.filter((user) => user.first_name.includes('tit')));
-})
-
+// All pairs of IDs for a game and user
 app.get('/gamesUsers', function (req, res) {
   res.set('Access-Control-Allow-Origin', '*');
   res.send(gamesUsers);
 })
 
+// User with specified ID
+app.get('/users/:userId', function (req, res) {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.send(users.find((user) => user.id == req.params.userId));
+})
+
+// Users with specified gender
+app.get('/users/gender/:userGender', function (req, res) {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.send(users.filter((user) => user.gender == req.params.userGender));
+})
+
+// Looking for users with tidies
+app.get('/users/find/tidies', function (req, res) {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.send(users.filter((user) => user.first_name.includes('tit')));
+})
+
+// Pairs of IDs for game and specified user (what games that user has)
 app.get('/gamesUsers/user/:id_user', function (req, res) {
   res.set('Access-Control-Allow-Origin', '*');
   res.send(gamesUsers.filter((gamesUsers) => gamesUsers.id_user == req.params.id_user));
+})
+
+// Pairs of IDs for specified game and user (which users have that game)
+app.get('/gamesUsers/game/:id_game', function (req, res) {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.send(gamesUsers.filter((gamesUsers) => gamesUsers.id_game == req.params.id_game));
+})
+
+// Game with specified ID
+app.get('/games/:gameId', function (req, res) {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.send(games.find((game) => game.id == req.params.gameId));
+})
+
+// Games with specified genre
+app.get('/games/genres/:genre', function (req, res) {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.send(games.find((game) => game.genre == req.params.genre));
 })
 
 app.listen(process.env.PORT || 8080, () => console.log("jestem gotuw na twojego fiuta, kogucie"))
